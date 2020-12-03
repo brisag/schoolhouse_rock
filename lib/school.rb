604d975d4da3e8e6fc6c12d8918c0db3810c280a
Brisa Garcia <brisagarciaglz@gmail.com>
2020-12-03 08:14:39 -0700

    Partial update

--- lib/school.rb
+++ lib/school.rb
@@ -1,32 +1,33 @@
+
 class School
   attr_reader :start_time,
               :hours,
-              :student_name
+              :student_names
 
   def initialize(start_time, hours)
     @start_time = start_time
     @hours = hours
+    @student_names = []
+ end
 
-end
 
-school = School.new('9:00', 7)
-
-def start_time (start_time)
+def it_has_a_start_time (start_time)
   @start_time = start_time
 end
 
-def hours_in_school_day(start_time, hours)
-  @start_time[0..1].to_i + time).to_s + ":00"
-  @hours = hours
 
+def hours_in_school_day(start_time, hours)
+  @start_time = start_time
+  @hours = hours
 end
 
-def student_names(student_name)
-  @student_name.push(student_name)
+def it_starts_with_no_student_names(student_names)
+end
 
+def it_can_add_student_names
+        @student_name.push(student_names)
+end
 
-
-  def is_full_time?
-    if @hours > 4 == true
-    end
+def it_can_calculate_end_time
+end
 end
